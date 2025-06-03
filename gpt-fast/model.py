6de3bcaa9eb64a06bbb521042d91a74ab4056d7e
dat-adi <dat.adithya@gmail.com>
2025-06-03 20:54:06 +0000

    fix: missing KVCache function
    I think I messed up the previous merge during rebase, so the KVCache
    was missing and the execution failed. This commit should fix that.

--- gpt-fast/model.py
+++ gpt-fast/model.py
@@ -116,6 +116,25 @@ def simulate_splitk(X, A, threshold, uuid):
     return Y
 
 
+class KVCache(nn.Module):
+    def __init__(self, max_batch_size, max_seq_length, n_heads, head_dim, dtype=torch.float16):
+        super().__init__()
+        cache_shape = (max_batch_size, n_heads, max_seq_length, head_dim)
+        self.register_buffer('k_cache', torch.zeros(cache_shape, dtype=dtype))
+        self.register_buffer('v_cache', torch.zeros(cache_shape, dtype=dtype))
+
+    def update(self, input_pos, k_val, v_val):
+        # input_pos: [S], k_val: [B, H, S, D]
+        assert input_pos.shape[0] == k_val.shape[2]
+
+        k_out = self.k_cache
+        v_out = self.v_cache
+        k_out[:, :, input_pos] = k_val
+        v_out[:, :, input_pos] = v_val
+
+        return k_out, v_out
+
+
 class Transformer(nn.Module):
     def __init__(self, config: ModelArgs) -> None:
         super().__init__()
